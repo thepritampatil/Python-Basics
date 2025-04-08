ed5283cbe0522a94f53599cf8625f502f0cedbd7
thepritampatil <thepritampatil@gmail.com>
2025-04-08 14:20:38 +0530

    loops in python

---
+++ loops.py
@@ -0,0 +1,15 @@
+# loops in python
+# loops are use to repeat instructions
+
+# while loop
+# repeat instructions as long as the condition is true
+i = 0
+while i < 5:
+    print(i)
+    i += 1
+    # output: 0, 1, 2, 3, 4
+
+# break and continue statements
+# else block in for loop
+# else block in while loop
+# for loop in python
